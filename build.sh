297ea370e7f8ae957312fd10409d3d5134307fa7
bbaumg <bbaumg@gmail.com>
2015-11-18 19:50:35 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 
-# Instructions:
-# Run the two below commands on the pi for the pinewood derby app.
-# curl "https://raw.githubusercontent.com/bbaumg/derby/master/build.sh" > build.sh
-# sudo bash build.sh
-
 #Variables
-log="/var/log/pibuild.log"
-v_repo='https://raw.githubusercontent.com/bbaumg/derby/master'
-v_defaultapps="python-pip python-dev python-smbus python-imaging i2c-tools vim git-core locate build-essential scons swig htop"
+log="build.log"
 v_gitEmail='bbaumg@gmail.com'
 v_gitUser='Barrett'
 
 # OK, let's install all of the basic stuff and do the basline configurations
 sudo raspi-config
-echo -en "\n-------------------------------------------------------\napt-get update\n\n" | tee -a $log
-sudo apt-get dist-upgrade -y
-echo -en "\n-------------------------------------------------------\napt-get install\n\n" | tee -a $log
-sudo apt-get update -y
 echo -en "\n-------------------------------------------------------\napt-get dist-upgrade\n\n" | tee -a $log
-sudo apt-get install -y $v_defaultapps
-echo -en "\n-------------------------------------------------------\npip3.2 install\n\n" | tee -a $log
-sudo pip install pyephem pymysql configparser
-
+sudo apt-get dist-upgrade -y
+echo -en "\n-------------------------------------------------------\napt-get update\n\n" | tee -a $log
+sudo apt-get update -y
+echo -en "\n-------------------------------------------------------\napt-get install\n\n" | tee -a $log
+sudo apt-get install -y python-pip python-dev python-smbus python-imaging i2c-tools vim git-core locate build-essential scons swig htop
+echo -en "\n-------------------------------------------------------\napt-get dist-config\n\n" | tee -a $log
+sudo apt-get dist-upgrade -y
+echo -en "\n-------------------------------------------------------\napt-get update\n\n" | tee -a $log
+sudo apt-get update -y
 
 echo -en "\n-------------------------------------------------------\nAdding to .bashrc\n\n" | tee -a $log
+cd /home/pi
 sed -i --follow-symlinks '/stuff/d' .bashrc
 sed -i --follow-symlinks '/alias ll/d' .bashrc
 sed -i --follow-symlinks '/export EDITOR/d' .bashrc
@@ -33,23 +28,44 @@ echo -en "\n# Some stuff I added\n"\
 "export EDITOR=vim\n" >> .bashrc
 
 echo -en "\n-------------------------------------------------------\nSettup MOTD\n\n" | tee -a $log
-curl "$v_repo/kickstart/banner" > /etc/banner
-curl "$v_repo/kickstart/motd.sh" > /etc/motd.sh
+cd /home/pi
+curl "https://raw.githubusercontent.com/bbaumg/scripts/master/kickstart/banner" | sudo tee /etc/banner
+curl "https://raw.githubusercontent.com/bbaumg/scripts/master/kickstart/motd.sh" | sudo tee /etc/motd.sh
 sed -i --follow-symlinks '/motd.sh/d' .bashrc
 echo '[ -n "$PS1" ] && bash /etc/motd.sh' >> .bashrc
 
-echo -en "\n-------------------------------------------------------\nSetting up git\n\n" | tee -a $log
-cd $HOME
-git config --global user.email "$v_gitEmail"
-git config --global user.name "$v_gitUser"
-
-echo -en "\n-------------------------------------------------------\nInstall Derby\n\n" | tee -a $log
-cd $HOME
+echo -en "\n-------------------------------------------------------\nGet Derby App\n\n" | tee -a $log
+cd /home/pi
 git clone https://github.com/bbaumg/derby.git
 #sudo bash derby/install.sh
 
-echo -en "\n-------------------------------------------------------\nRemove Install File\n\n" | tee -a $log
-cd $HOME
-sudo rm -f build.sh
 
-echo -en "\n-------------------------------------------------------\nBuild Complete\n\n" | tee -a $log
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
